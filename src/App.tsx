e828e8220b32d12bc1c9fd56f57cdac12c4e6535
ismaelsilvat <ismaelteixeiradas1@hotmail.com>
2022-12-18 13:18:55 -0300

    Initial Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ConfigProvider, theme } from "antd";
+import "antd/dist/reset.css";
+import { TitleComponent } from "./Test";
 
-function App() {
+const { useToken } = theme;
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "pink",
+          },
+        }}
+      >
+        <TitleComponent />
+        <Router></Router>
+      </ConfigProvider>
+    </>
   );
-}
-
-export default App;
+};
